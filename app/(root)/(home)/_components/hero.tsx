445edaf4a07de2ce4c4bf3e76bd35ded78ad456f
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2024-11-28 11:04:17 +0900

    Update hero section adding personal links

--- app/(root)/(home)/_components/hero.tsx
+++ app/(root)/(home)/_components/hero.tsx
@@ -1,19 +1,42 @@
 import { Button } from '@/components/ui/button'
+import { Github, Linkedin } from 'lucide-react'
 import Link from 'next/link'
+import { MdEmail } from 'react-icons/md'
 
 function Hero() {
 	return (
 		<>
 			<div className='mx-auto flex justify-center min-h-screen max-w-6xl gap-8 text-center max-md:mt-32'>
-				<div className='w-[500px] flex flex-col space-y-4 self-center'>
+				<div className='w-[700px] flex flex-col space-y-4 self-center'>
 					<h1 className='font-serif text-3xl font-bold'>
-						I am Nuriddinov Shokhrukh
+						I am <span className='text-blue-500'>Nuriddinov Shokhrukh</span>
 					</h1>
 					<p className='text-muted-foreground'>
-						Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-						Repudiandae quam minima, earum atque ipsam quo ipsum voluptates ab
-						ut, fuga debitis similique.
+						I solve problems and create captivating applications.
 					</p>
+					<p className='text-muted-foreground'>
+						I am a{' '}
+						<span className='text-blue-500'>Full Stack Web Developer</span>{' '}
+						based in Seoul, South Korea, deriving immense satisfaction from
+						solving intricate problems through the art of coding!
+					</p>
+					<div className='flex justify-center items-center gap-8'>
+						<Button type='button' size={'icon'} className='rounded-full'>
+							<Link href={'https://github.com/frontend-tester02'}>
+								<Github />
+							</Link>
+						</Button>
+						<Button type='button' size={'icon'} className='rounded-full'>
+							<Link href={'https://github.com/frontend-tester02'}>
+								<Linkedin />
+							</Link>
+						</Button>
+						<Button type='button' size={'icon'} className='rounded-full'>
+							<Link href={'https://github.com/frontend-tester02'}>
+								<MdEmail />
+							</Link>
+						</Button>
+					</div>
 					<div className='flex justify-center gap-4'>
 						<Link href={'/courses'}>
 							<Button variant={'outline'} size={'lg'} className='rounded-full'>
